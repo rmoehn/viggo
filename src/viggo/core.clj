b6e7ecacd0d3c6cfc8cc7e5eb71a33e595f49e96
Richard Möhn <richard.moehn@fu-berlin.de>
2014-08-13 10:15:15 +0200

    Start with image page generation
    Of course I would never generate an HTML page like that, but my goal was
    to write some Clojure every day in order to gain exposure to the
    language and get used to programming again. It was not digging around in
    documentation. The next step will of course be doing all that with
    Enlive and proper HTML5.

--- src/viggo/core.clj
+++ src/viggo/core.clj
@@ -1,4 +1,46 @@
-(ns viggo.core)
+(ns viggo.core
+  (:require [clojure.java.io :as io]))
+
+(def html-doc-start
+  (str "<!DOCTYPE html>"
+       "<html lang=\"en\">"
+       "<head>"
+       "  <meta charset=\"utf-8\"/>"
+       "</head>"
+       "<body>"))
+
+(def html-doc-end
+  (str "</body>"
+       "</html>"))
+
+(defn gen-img-directive
+  "Takes an image file and returns a HTML directive for including that image
+  in a web page."
+  [file]
+  (str "<img src=\"" (io/as-url file) "\">" (.getName file) "</img>"))
+
+(defn gen-paragraph
+  "Returns arbitrary html wrapped in paragraph tags."
+  [html-str]
+  (str "<p>" html-str "</p>\n\n"))
+
+(def add-newline
+  #(str % "\n"))
+
+(defn gen-image-list
+  [image-files]
+  (map #(gen-paragraph
+          (add-newline
+            (gen-img-directive %)))
+       image-files))
+
+(def image-files
+  (file-seq
+    (io/file
+      (io/resource "pictures"))))
+
+(def image-page
+  (str html
 
 (defn foo
   "I don't do a whole lot."
