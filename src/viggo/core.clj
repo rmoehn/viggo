235026fb852bac848005416178e3d5ec2c083a5d
Richard Möhn <richard.moehn@fu-berlin.de>
2014-08-14 10:45:42 +0200

    Finish cobbling together image page
    It took me some time to figure out how to serve static content, but now
    it works.

--- src/viggo/core.clj
+++ src/viggo/core.clj
@@ -1,48 +1,68 @@
 (ns viggo.core
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [ring.util.response :refer [response]]
+            [ring.middleware.file :refer [wrap-file]]
+            [compojure.core :refer :all]))
 
-(def html-doc-start
-  (str "<!DOCTYPE html>"
-       "<html lang=\"en\">"
-       "<head>"
-       "  <meta charset=\"utf-8\"/>"
-       "</head>"
-       "<body>"))
+(def picture-resource (io/resource "pictures"))
 
-(def html-doc-end
-  (str "</body>"
-       "</html>"))
+(def image-files
+  (filter #(.isFile %)
+          (file-seq
+            (io/file
+              (io/resource "pictures")))))
 
-(defn gen-img-directive
-  "Takes an image file and returns a HTML directive for including that image
-  in a web page."
-  [file]
-  (str "<img src=\"" (io/as-url file) "\">" (.getName file) "</img>"))
+(def add-newline
+  #(str % "\n"))
+
+(defn make-lines
+  "Return a multi-line string made from the string arguments."
+  [& strs]
+  (string/join \newline strs))
 
 (defn gen-paragraph
   "Returns arbitrary html wrapped in paragraph tags."
   [html-str]
   (str "<p>" html-str "</p>\n\n"))
 
-(def add-newline
-  #(str % "\n"))
+(def html-doc-start
+  (make-lines
+     "<!DOCTYPE html>"
+     "<html lang=\"en\">"
+     "<head>"
+     "  <meta charset=\"utf-8\"/>"
+     "</head>"
+     "<body>"))
+
+(def html-doc-end
+  (make-lines
+     "</body>"
+     "</html>"))
+
+(defn gen-img-directive
+  "Takes an image file and returns a HTML directive for including that image
+  in a web page."
+  [file]
+  (let [basename (.getName file)]
+    (str "<img src=\"" basename "\">" basename "</img>")))
 
 (defn gen-image-list
   [image-files]
   (map #(gen-paragraph
-          (add-newline
-            (gen-img-directive %)))
+          (gen-img-directive %))
        image-files))
 
-(def image-files
-  (file-seq
-    (io/file
-      (io/resource "pictures"))))
-
 (def image-page
-  (str html
+  (make-lines
+    html-doc-start
+    (string/join (gen-image-list image-files))
+    html-doc-end))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes image-handler
+  (ANY "/" [] image-page))
+
+(def app (wrap-file image-handler (.getPath picture-resource)))
+
+(defonce server (run-jetty #'app {:port 10000 :join? false}))
