900ef9380898d1ee6ba9c6caae89938da34159dc
Richard Möhn <richard.moehn@fu-berlin.de>
2014-08-16 21:08:34 +0200

    Add midje dependency, delete core_test

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-core "1.3.0"]
                  [ring/ring-jetty-adapter "1.3.0"]
-                 [compojure "1.1.8"]])
+                 [compojure "1.1.8"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
