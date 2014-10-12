79ed339571aba4317153e58b8a17f580945a35fc
Richard Möhn <richard.moehn@fu-berlin.de>
2014-10-12 15:46:13 +0200

    Start introducing Enlive
    Constructing HTML in the code was more a joke from the beginning. Remove
    that and start with Enlive.

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-core "1.3.0"]
                  [ring/ring-jetty-adapter "1.3.0"]
-                 [compojure "1.1.8"]]
+                 [compojure "1.1.8"]
+                 [enlive "1.1.5"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]
                                   [com.google.guava/guava "13.0.1"]]}})
--- src/viggo/core.clj
+++ src/viggo/core.clj
@@ -4,7 +4,8 @@
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.util.response :refer [response]]
             [ring.middleware.file :refer [wrap-file]]
-            [compojure.core :refer :all]))
+            [compojure.core :refer :all]
+            [net.cgrand.enlive-html :as html]))
 
 (def picture-resource (io/resource "pictures"))
 
@@ -14,54 +15,11 @@
             (io/file
               (io/resource "pictures")))))
 
-(def add-newline
-  #(str % "\n"))
-
-(defn make-lines
-  "Return a multi-line string made from the string arguments."
-  [& strs]
-  (string/join \newline strs))
-
-(defn gen-paragraph
-  "Returns arbitrary html wrapped in paragraph tags."
-  [html-str]
-  (str "<p>" html-str "</p>\n\n"))
-
-(def html-doc-start
-  (make-lines
-     "<!DOCTYPE html>"
-     "<html lang=\"en\">"
-     "<head>"
-     "  <meta charset=\"utf-8\"/>"
-     "</head>"
-     "<body>"))
-
-(def html-doc-end
-  (make-lines
-     "</body>"
-     "</html>"))
-
-(defn gen-img-directive
-  "Takes an image file and returns a HTML directive for including that image
-  in a web page."
-  [file]
-  (let [basename (.getName file)]
-    (str "<img src=\"" basename "\">" basename "</img>")))
-
-(defn gen-image-list
-  [image-files]
-  (map #(gen-paragraph
-          (gen-img-directive %))
-       image-files))
-
-(def image-page
-  (make-lines
-    html-doc-start
-    (string/join (gen-image-list image-files))
-    html-doc-end))
+(html/deftemplate image-page (io/resource "templates/pic_list.html") [])
 
 (defroutes image-handler
-  (ANY "/" [] image-page))
+  (ANY "/" [] (image-page)))
+;; route/resources
 
 (def app (wrap-file image-handler (.getPath picture-resource)))
 
