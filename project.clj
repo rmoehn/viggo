733122b9f50e87242aef95c80535043e619dc66b
Richard Möhn <richard.moehn@fu-berlin.de>
2014-10-13 18:47:54 +0200

    Update information in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject viggo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A web application for assembling communication books"
+  :url "https://github.com/rmoehn/viggo"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-core "1.3.0"]
                  [ring/ring-jetty-adapter "1.3.0"]
